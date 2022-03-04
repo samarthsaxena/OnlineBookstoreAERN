9dd28a42cd56c74cf0d589d05e4170054072a29e
SaSaxena <samarth.saxena@microfocus.com>
2022-03-04 09:09:31 +0530

    added welcome to / route

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const { request } = require('express');
 const bookstall = require('./bookstall');
 
+router.get("/", (req, res) => {
+    res.send("<h2>Welcome to home page</h2>");
+})
 // If requested resource is /api then it wil be routed to bookstall.js
 router.use("/api", bookstall);
 
